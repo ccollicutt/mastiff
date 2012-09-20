d9d2dd41c54c83352b67d86c29cc12743d12a303
curtis <curtis@cloud01.private.vclcloud.cybera.ca>
2012-09-20 14:14:50 -0600

    added example yml file for testing

--- mastiff.py
+++ mastiff.py
@@ -4,7 +4,12 @@ import re
 import sys
 import os, os.path
 import ConfigParser
-from novaclient.v1_1 import client
+
+try:
+    from novaclient.v1_1 import client
+except ImportError:
+    print >>sys.stderr, 'ERROR: requires python novaclient'
+    sys.exit(1)
 
 try:
     import argparse
@@ -24,6 +29,13 @@ try:
 except ImportError:
     print >>sys.stderr, 'ERROR: requires ansible'
     sys.exit(1)
+
+try: 
+    import yaml
+except ImportError:
+    print >>sys.stderr, "ERROR: requires pyaml"
+    sys.exit(1)
+
 
 CONFIG_FILE = "./mastiff.conf"
 
@@ -97,6 +109,13 @@ def main(args):
         default=False
     )
 
+    parser.add_argument(
+        "--breed", 
+        help="Set what group of servers to build from the config file",
+        action="store",
+        dest="BREED",
+    )
+
     #
     # Done with arguments
     #---------------------------------------------------------------------------
@@ -126,11 +145,60 @@ def main(args):
         print >>sys.stderr, "ERROR: Could not connect to OpenStack via nova python client"
         sys.exit(1)
 
+   
+    if args.BREED:
+        BREED = args.BREED
+    else:
+       # nothing to do
+       sys.exit(0)
+
+    try:
+        stream = open(BREED + ".yml", 'r')
+    except:
+        print >>sys.stderr, "ERROR: Failed to open " + BREED + ".yml"
+        sys.exit(1)  
+
+    try:
+        breed =  yaml.load(stream)
+    except:
+        print >>sys.stderr, "ERROR: " + BREED + ".yml is not a valid yaml file"
+        sys.exit(1) 
+
     if args.SHELL:
         #ipython_shell(nova)
         ipshell=InteractiveShellEmbed()
         ipshell()
-   
+        print "Exiting IPython shell..."
+        sys.exit(0)
+
+    instances = breed[str(BREED)]
+    servers = []
+
+    for i, k in instances.iteritems():
+        for item in k:
+            if 'instances' in item:
+                num_instances = item['instances']
+            elif 'image' in item:
+                image = item['image']
+            elif 'playbook' in item:
+                playbook = item['playbook']
+            elif 'flavor' in item:
+                flavor = item['flavor']
+            else:
+                flavor = "1"
+
+        # Don't forget flavor XXX
+        if num_instances is not None and image is not None:
+            for j in range(int(num_instances)):
+                server = nova.servers.create(flavor=flavor,image=image,name=str(BREED) + ' ' + str(i) + ' ' + str(j))
+                if server:
+                    servers.append(server)
+        else:
+            print "no num_instances and image"
+
+    for server in servers:
+        print server.id
+
     sys.exit(0)
     
 if __name__ == '__main__':
